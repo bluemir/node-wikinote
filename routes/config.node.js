3c0759089faae2eb3b136d4ec951b260aaa7717b
BlueMir <qa22ahj@gmail.com>
2013-08-06 09:40:03 +0900

    add default config

--- routes/config.node.js
+++ routes/config.node.js
@@ -1,8 +1,8 @@
 var config = require("../config.json");
 var env = process.env;
 
-config.wikiDir = config.wikiDir || "~/wiki"
+config.frontPage = config.frontPage || "FrontPage";
+config.wikiDir = config.wikiDir || "~/wiki";
 config.wikiDir = config.wikiDir.replace(/^~/g, env.HOME);
-
 
 module.exports = config;
