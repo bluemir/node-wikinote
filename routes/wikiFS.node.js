4dd23ef767ebf0644b0e6f6cbb846e03f912af30
BlueMir <qa22ahj@gmail.com>
2014-04-10 23:02:37 +0900

    refactor wikiFS.js : use mkdirp

--- routes/wikiFS.node.js
+++ routes/wikiFS.node.js
@@ -1,14 +1,13 @@
 var fs = require("fs");
+var mkdirp = require("mkdirp");
 var exec = require("child_process").exec;
 var config = require("./config.node.js");
 
-var saveDir = config.wikiDir;
-
 var SearchEngine = require("./searchEngine.node.js")
-var searchEngine = new SearchEngine(saveDir);
+var searchEngine = new SearchEngine(config.wikiDir);
 
 exports.readWiki = function(path, callback){
-	fs.readFile(saveDir + path.full + ".md", "utf8", function(err, data){
+	fs.readFile(config.wikiDir + path.full + ".md", "utf8", function(err, data){
 		if(err){
 			callback(err);
 			return;
@@ -17,8 +16,8 @@ exports.readWiki = function(path, callback){
 	});
 }
 exports.writeWiki = function(path, data, author, callback){
-	readyDir(path, function (){
-		fs.writeFile(saveDir + path.full + ".md", data, "utf8", function(err){
+	mkdirp(config.wikiDir + path.toString(), function(){
+		fs.writeFile(config.wikiDir + path.full + ".md", data, "utf8", function(err){
 			if(!err)
 				backup("update", path.full, author, callback);
 			else
@@ -27,28 +26,28 @@ exports.writeWiki = function(path, data, author, callback){
 	})
 }
 exports.fileList = function(path, callback){
-	fs.readdir(saveDir + path.full, callback);
+	fs.readdir(config.wikiDir + path.full, callback);
 }
 exports.acceptFile  = function(srcPath, path, name, callback){
-	readyDir(path, function(){
+	mkdirp(config.wikiDir + path.toString(), function(){
 		fs.readFile(srcPath, function(e, data){
 			if(e) return callback(e);
-			fs.writeFile(saveDir + path.full + "/" + name, data, callback);
+			fs.writeFile(config.wikiDir + path.full + "/" + name, data, callback);
 		});
 	});
 }
 exports.deleteFile = function(path, callback){
-	fs.unlink(saveDir + path, function(e){
+	fs.unlink(config.wikiDir + path, function(e){
 		console.log(e);
-		fs.unlink(saveDir + path + ".md", callback );
+		fs.unlink(config.wikiDir + path + ".md", callback );
 	});
 }
 exports.move = function(srcPath, targetPath, callback){
-	readyDir(targetPath.path, function(e){
-		fs.rename(saveDir + srcPath.full, saveDir + targetPath.full,function(e){
+	mkdirp(config.wikiDir + path.toString(), function(){
+		fs.rename(config.wikiDir + srcPath.full, config.wikiDir + targetPath.full,function(e){
 			console.log(e);
 		});
-		fs.rename(saveDir + srcPath.full + ".md", saveDir + targetPath.full + ".md", function(e){
+		fs.rename(config.wikiDir + srcPath.full + ".md", config.wikiDir + targetPath.full + ".md", function(e){
 			console.log(e);
 			callback(null);
 		});
@@ -64,8 +63,8 @@ exports.history = function(path, callback){
 	//%an : author name
 	//%ae : author email
 	var logFormat = "%cD%x01%s%x01%h%x01%an%x01%ae"
-	var command = "git log --pretty=format:'" + logFormat + "' -- " + saveDir + path + ".md";
-	exec(command, {cwd : saveDir}, function(e, stdout, stderr){
+	var command = "git log --pretty=format:'" + logFormat + "' -- " + config.wikiDir + path + ".md";
+	exec(command, {cwd : config.wikiDir}, function(e, stdout, stderr){
 		var logs = stdout.split(/[\n\r]/g).map(function(log, index){
 			var tmp = log.split("\x01");
 			return { 
@@ -81,36 +80,6 @@ exports.history = function(path, callback){
 		callback(null, logs);
 	});
 } 
-function readyDir(path, callback){
-	var pathArray = path.toArray();
-
-	checkDir(pathArray.length, function(err, level){
-		if(err) return callback(err);
-
-		mkdir(level + 1, callback);
-	});
-
-	function checkDir(level, callback){
-		if(level < 0) return callback(new Error());
-
-		fs.exists(getPath(level), function(exist){
-			if(exist) return callback(null, level);
-			checkDir(level - 1, callback);
-		});
-	}
-
-	function mkdir(level, callback){
-		if(level > pathArray.length) return callback(null);
-
-		fs.mkdir(getPath(level), function(err){
-			mkdir(level + 1, callback);
-		});
-	}
-
-	function getPath(level){
-		return saveDir + "/" + pathArray.slice(0, level).join("/");
-	}
-}
 function backup(method, fullname, author, callback){
 	if(!config.autoBackup){
 		callback();
@@ -124,7 +93,7 @@ function backup(method, fullname, author, callback){
 	command += "-m" + wapper(buildMessage(method, fullname));
 	command += "--author" + wapper(signature(author));
 
-	exec(command, {cwd : saveDir}, function(e, stdout, stderr){
+	exec(command, {cwd : config.wikiDir}, function(e, stdout, stderr){
 		callback();			
 	});
 	
