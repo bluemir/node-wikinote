d09ad9a3204d95a84dd62d2aaa6f5df59ebf07c2
BlueMir <qa22ahj@gmail.com>
2014-06-28 03:00:51 +0900

    remove unused lib

---
+++ public/presentation.js
@@ -0,0 +1,39 @@
+function init(theme, transition){
+	// Full list of configuration options available here:
+	// https://github.com/hakimel/reveal.js#configuration
+	Reveal.initialize({
+		controls: true,
+		progress: true,
+		history: true,
+		center: true,
+
+		theme: theme,
+		//|| Reveal.getQueryHash().theme, // available themes are in /css/theme
+
+		transition: transition,
+		//|| Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+		// Optional libraries used to extend on reveal.js
+		dependencies: [ { 
+				src: '/!public/lib/reveal.js/lib/js/classList.js', 
+				condition: function() { return !document.body.classList; } 
+			}, { 
+				src: '/!public/lib/reveal.js/plugin/markdown/showdown.js', 
+				condition: function() { return !!document.querySelector( '[data-markdown]' );}
+			}, { 
+				src: '/!public/lib/reveal.js/plugin/markdown/markdown.js', 
+				condition: function() { return !!document.querySelector( '[data-markdown]' );}
+			}, { 
+				src: '/!public/lib/reveal.js/plugin/highlight/highlight.js', 
+				async: true, 
+				callback: function() { hljs.initHighlightingOnLoad(); }
+			}, { 
+				src: '/!public/lib/reveal.js/plugin/zoom-js/zoom.js', 
+				async: true,
+				condition: function() { return !!document.body.classList; }
+			}, { 
+				src: '/!public/lib/reveal.js/plugin/notes/notes.js', 
+				async: true, condition: function() { return !!document.body.classList; }
+			}
+		]
+	});
+}
