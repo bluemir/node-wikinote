301c7b5a7a840f15e90acf1feb95c7380d701acc
bluemir <qa22ahj@gmail.com>
2016-04-05 14:22:31 +0900

    Update entry.sh : save default configuration when start

--- docker/entry.sh
+++ docker/entry.sh
@@ -6,9 +6,22 @@ if [ ! -d /wikinote/data/.git ]; then
 		git init && \
 		git config user.name anonymous && \
 		git config user.email anonymous@wikinote && \
-		git commit -m "Initial commit" --allow-empty
+		git commit -m "Initial commit" --allow-empty && \
+		echo ".app/" >> .gitignore
 fi
 
-cd /wikinote/src && \
-	node wikinote.js
+if [ ! -d /wikinote/data/.app/config.yaml ]; then
+	cd /wikinote/src && \
+		node wikinote.js  \
+			--port 4000 \
+			--wikinote-path /wikinote/data \
+			--wikiname WikiNote \
+			--front-page front-page \
+			--save
+else
+	cd /wikinote/src && \
+		node wikinote.js \
+			--port 4000 \
+			--wikinote-path /wikinote/data
+fi
 
